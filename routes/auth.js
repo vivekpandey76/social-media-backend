46ec3eb2b91cf1703db078b5365b7022a12179c7
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-21 19:21:29 +0530

    Register Backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("Hello Bdsdsuh!");
+router.post("/register", async (req, res) => {
+  try {
+    //creating our password in machine language so no one can check password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // creating a new user when they register
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
