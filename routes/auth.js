cb106ed1086a716fe4cf83de58544da98ecfd345
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-21 20:00:54 +0530

    Login and register

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+//Register
 router.post("/register", async (req, res) => {
   try {
     //creating our password in machine language so no one can check password
@@ -18,7 +19,24 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    //Compare login email with register email
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User Not Found");
+    //Compare password with register password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
