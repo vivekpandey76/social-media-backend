9105f54dec13014255a7a297f670b29446fd1089
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-25 13:26:38 +0530

    Done with rest api

--- routes/posts.js
+++ routes/posts.js
@@ -42,7 +42,7 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-//Liked a post
+//Liked/disked a post
 
 router.put("/:id/like", async (req, res) => {
   try {
@@ -59,6 +59,28 @@ router.put("/:id/like", async (req, res) => {
   }
 });
 //Get a post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //get all post of user following
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
