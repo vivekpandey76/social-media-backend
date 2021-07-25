33efa6145906d83c065c88680c44d9c518f83110
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-25 12:57:49 +0530

    Likes Backend

--- routes/posts.js
+++ routes/posts.js
@@ -43,6 +43,21 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //Liked a post
+
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("the post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //Get a post
 //get all post of user following
 
