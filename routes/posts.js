32a0de30aa1764273484fda06b5a53b40cce1236
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-25 11:04:56 +0530

    Delete Post Backend

--- routes/posts.js
+++ routes/posts.js
@@ -11,8 +11,37 @@ router.post("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
 //update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post has been updated");
+    } else {
+      res.status(403).json("You cannot update someoneElse id");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //Delete a post
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("Post has been deleted");
+    } else {
+      res.status(403).json("You cannot Delete someonelse post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //Liked a post
 //Get a post
 //get all post of user following
